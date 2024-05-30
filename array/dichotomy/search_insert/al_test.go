196c95d91e5d64554d357e5878575783db1e00aa
huangyul <1103221645@qq.com>
2024-05-30 16:16:13 +0800

    35.搜索插入位置

---
+++ array/dichotomy/search_insert/al_test.go
@@ -0,0 +1,42 @@
+package searchinsert
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_searchInsert(t *testing.T) {
+	testCases := []struct {
+		name   string
+		nums   []int
+		target int
+		output int
+	}{
+		{
+			name:   "test 1",
+			nums:   []int{1, 3, 5, 6},
+			target: 5,
+			output: 2,
+		},
+		{
+			name:   "test 2",
+			nums:   []int{1, 3, 5, 6},
+			target: 2,
+			output: 1,
+		},
+		{
+			name:   "test 3",
+			nums:   []int{1, 3, 5, 6},
+			target: 7,
+			output: 4,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := searchInsert(tc.nums, tc.target)
+			assert.Equal(t, tc.output, res)
+		})
+	}
+}
