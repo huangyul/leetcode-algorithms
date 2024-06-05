bc6c315a9157f67cc2c0c5fc86c4df1536ec488a
huangyul <1103221645@qq.com>
2024-06-05 15:05:17 +0800

    904.水果成篮

--- array/tow-pointer/total-fruit/al.go
+++ array/tow-pointer/total-fruit/al.go
@@ -6,23 +6,22 @@ package totalfruit
 func totalFruit(nums []int) int {
 	maxLen := 0
 	s, f := 0, 0
-	count := make(map[int]int)
-
-	for f <= len(nums)-1 {
-		count[nums[f]]++
-		// 如果超过两种水果，就缩小边界
-		for len(count) > 2 {
-			count[nums[s]]--
-			if count[nums[s]] == 0 {
-				delete(count, nums[s])
+	m := make(map[int]int)
+	for f < len(nums) {
+		m[nums[f]]++
+		for len(m) > 2 {
+			m[nums[s]]--
+			if m[nums[s]] == 0 {
+				delete(m, nums[s])
 			}
 			s++
 		}
-		if maxLen < (f - s + 1) {
-			maxLen = f - s + 1
+		if (f - s + 1) > maxLen {
+			maxLen = (f - s + 1)
 		}
 		f++
 	}
 
 	return maxLen
+
 }
